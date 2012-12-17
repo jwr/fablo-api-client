4895a1af198ea38f126088ba09297e27555e6898
Kamil Szymczyk <kaszymczyk@gmail.com>
2012-12-17 15:44:21 +0100

    Added command line front to library.

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject fablo/api-client "1.1.2"
+(defproject fablo/api-client "1.2.0"
+  :main fablo.api-client-main
   :description "Fablo API client"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-json "0.5.0"]
                  [commons-io/commons-io "2.0.1"]
                  [commons-codec "1.4"]
                  [clj-http "0.3.6"]
-                 [ring/ring-core "0.3.11"]]
+                 [ring/ring-core "0.3.11"]
+                 [clojopts "0.3.2"]]
   :dev-dependencies [[swank-clojure "1.4.2"]])
