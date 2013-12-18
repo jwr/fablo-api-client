3c639a85934005037a2e919f5939bb2661419cda
Jan Rychter <jan@rychter.com>
2013-12-18 13:22:32 +0100

    fix how the '+' character is handled in URL-encoding

--- src/fablo/auth.clj
+++ src/fablo/auth.clj
@@ -22,13 +22,47 @@
   ([str k v] `(string/replace ~str ~k ~v))
   ([str k v & args] `(replace-all (string/replace ~str ~k ~v) ~@args)))
 
-;; Java's url-encoder has a bit different semantics than the one
-;; mandated by AWS, so we provide a translation layer:
-(defn url-encode-with-aws-semantics [s]
-  (replace-all (codec/url-encode s)
-               "*" "%2A"
-               "%7E" "~"
-               "+" "%20"))
+;; There is a lot of confusion around what needs to be percent-encoded for AWS and what should not be. Most descriptions
+;; are unfortunately imprecise. The best description seems to be at
+;; http:/docs.aws.amazon.comĄmazonSimpleDBłatest/DeveloperGuide/HMACAuth.html, stating:
+;;
+;; * Do not URL encode any of the unreserved characters that RFC 3986 defines. These unreserved characters are A-Z, a-z,
+;;   0-9, hyphen ( -), underscore ( _), period ( .), and tilde ( ~).
+;; * Percent encode all other characters with %XY, where X and Y are hex characters 0-9 and uppercase A-F.
+;; * Percent encode the space character as %20 (and not +, as common encoding schemes do).
+;;
+;; This means we cannot use java.net.URLEncoder.encoder, nor ring.util.codec/url-encode, nor ring.util.codec/form-encode.
+;;
+;; This is an area where most software seems to be buggy. RFC3986 is imprecise, and how you encode a string depends on
+;; where that string is used. What this all means is that there is no such thing as "URL encoding".
+;;
+;; Here are the differences between various encodings:
+;;
+;; (import [java.net URLEncoder])
+;; (URLEncoder/encode "a b+c~-" "UTF-8")
+;; "a+b%2Bc%7E-"
+;;
+;; (ring.util.codec/form-encode "a b+c~-")
+;; "a+b%2Bc%7E-"
+;;
+;; (ring.util.codec/url-encode "a b+c~-")
+;; "a%20b+c~-"
+;;
+;; (ring.util.codec/url-encode-for-aws "a b+c~-")
+;; "a%20b%2Bc~-"
+;;
+;; The last one is what AWS requires. We call it 'url-encode-with-aws-semantics', because even though it is a strict
+;; reading of section 2.3 of RFC3986 (any character that isn't listed as Unreserved should be encoded), most software
+;; does not agree.
+
+(defn url-encode-with-aws-semantics
+  "Returns the url-encoded (as per section 2.3 of RFC3986) version of the given string, using either a specified
+  encoding or UTF-8 by default."
+  [unencoded & [encoding]]
+  (string/replace
+    unencoded
+    #"[^A-Za-z0-9_~.-]+"
+    #(ring.util.codec/percent-encode % encoding)))
 
 (defn canonicalize-query-string [req]
   (let [names-and-values (remove #(= (first %) "Signature") (:params req))
